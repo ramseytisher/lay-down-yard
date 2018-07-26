42231bc13eb978373c3e07ac94d59993234d1fdd
Tisher,Ramsey <RT022748@cerner.net>
2018-07-26 13:39:53 -0400

    fixed map

--- src/App.js
+++ src/App.js
@@ -11,7 +11,15 @@ class App extends Component {
           <h1 className="App-title">Website Under Construction</h1>
         </header>
         <div>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3558.0039667211568!2d-81.53632568445603!3d39.76588490288757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8837d3fdc5d706bb%3A0x9816db9814b2d51d!2sQuarry+Brothers+LLC+Properties!5e1!3m2!1sen!2sus!4v1532626206009" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3558.0039667211568!2d-81.53632568445603!3d39.76588490288757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8837d3fdc5d706bb%3A0x9816db9814b2d51d!2sQuarry+Brothers+LLC+Properties!5e1!3m2!1sen!2sus!4v1532626206009" 
+            title="lay-down-yard-location"
+            width="100%" 
+            height="600" 
+            frameborder="0" 
+            style={{ border: 0 }} 
+            allowfullscree
+            />
+
         </div>
       </div>
     );
